1e1fe5aedb1c2a2d127b53f5cbe0121c67d55a64
Aman Kumar <105852549+astroheree@users.noreply.github.com>
2023-07-13 15:35:00 +0530

    Create Majority Element II.cpp

---
+++ Majority Element II.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+
+        int ele1 = -1, ele2 = -1, cnt1 = 0, cnt2 = 0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(cnt1 == 0 && ele2 != nums[i])
+            {
+                cnt1 = 1;
+                ele1 = nums[i];
+            }
+            else if(cnt2 == 0 && ele1 != nums[i])
+            {
+                cnt2 = 1;
+                ele2 = nums[i];
+            }
+            else if(nums[i] == ele1) cnt1++;
+            else if(nums[i] == ele2) cnt2++;
+            else {cnt1--,cnt2--;}
+        }
+        cnt1 = 0, cnt2 = 0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(nums[i] == ele1) cnt1++;
+            else if(nums[i] == ele2) cnt2++;
+        }
+        if(cnt1 >= (n/3)+1) ans.push_back(ele1);
+        if(cnt2 >= (n/3)+1) ans.push_back(ele2);
+        return ans;
+    }
+};
