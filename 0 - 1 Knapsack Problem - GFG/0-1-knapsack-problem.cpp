0a5327b3688d9a8c182ef6c1b696d3ac907014bb
Aman Kumar <105852549+astroheree@users.noreply.github.com>
2023-07-14 23:48:59 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -7,27 +7,44 @@ using namespace std;
 class Solution
 {
     public:
-    int dp[1005][1005];
+    // int dp[1005][1005];
     //Function to return max value that can be put in knapsack of capacity W.
-    int solve(int i, int W, int wt[], int val[], int n)
-    {
-        if(i == 0)
-        {
-            if(wt[0] <= W) return val[0];
-            else return 0;
-        }
-        if(dp[i][W] != -1) return dp[i][W];
-        int pick = -1e9;
-        if(W >= wt[i])
-            pick = val[i] + solve(i-1,W-wt[i],wt,val,n);
+    // int solve(int i, int W, int wt[], int val[], int n)
+    // {
+    //     if(i == 0)
+    //     {
+    //         if(wt[0] <= W) return val[0];
+    //         else return 0;
+    //     }
+    //     if(dp[i][W] != -1) return dp[i][W];
+    //     int pick = -1e9;
+    //     if(W >= wt[i])
+    //         pick = val[i] + solve(i-1,W-wt[i],wt,val,n);
             
-        int notPick = solve(i-1,W,wt,val,n);
-        return dp[i][W] = max(pick,notPick);
-    }
+    //     int notPick = solve(i-1,W,wt,val,n);
+    //     return dp[i][W] = max(pick,notPick);
+    // }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-        memset(dp,-1,sizeof(dp));
-        return solve(n-1,W,wt,val,n);
+        vector<vector<int>> dp(n+1,vector<int>(W+1,0));
+        for(int i = 0 ; i <= W ; i++)
+        {
+            if(wt[0] <= i) dp[0][i] = val[0];
+        }
+        for(int i = 1 ; i < n ; i++)
+        {
+            for(int w = 0 ; w <= W ; w++)
+            {
+                int pick = -1e9;
+                if(w >= wt[i])
+                    pick = val[i] + dp[i-1][w-wt[i]];
+                    
+                int notPick = dp[i-1][w];
+                dp[i][w] = max(pick,notPick);
+            }
+        }
+        return dp[n-1][W];
+        // return solve(n-1,W,wt,val,n);
     }
 };
 
